ef101ee7a9ef35ce7c8bdd72a4498607300d2e14
Jacob Eisner <jteisner@email.unc.edu>
2023-02-09 19:24:23 -0500

    var name adjustment

--- cli.js
+++ cli.js
@@ -45,9 +45,9 @@ if (args.h) {
    console.log("tomorrow.")
  }
 
- if (data.daily.precipitation_hours[days] != 0) {
+ if (data.daily.precipitation_hours[day] != 0) {
 	console.log("You might need your galoshes ");
-} else if (data.daily.precipitation_hours[days] == 0) {
+} else if (data.daily.precipitation_hours[day] == 0) {
 	console.log("You will not need your galoshes ");
 }
 
